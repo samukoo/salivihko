31151ebbe1a4231b6f74482fd540b87c6add32ae
samukoo <samuk@kapsi.fi>
2014-04-27 19:07:55 +0300

    1. uusi_treeni sivun app_nv.js: 	a) lisätty for rakenteeseen if-else tarkistus, joka lisää ainoastaan kerran liikkeen nimen, ja sen alle kaikki liikkeen sarjat

--- workout/static/js/app.js
+++ workout/static/js/app.js
@@ -3,7 +3,7 @@
 $.getJSON('http://localhost:8080/workouts', function(data) {
     	
     	var output="</ul>";
-
+    	
     	for(i=0; i < data.result.length ; i++)
     	{	
     	output+= data.result[i].id+ ": " + data.result[i].workout+"<br>";
--- workout/static/js/app_nw.js
+++ workout/static/js/app_nw.js
@@ -1,11 +1,20 @@
 
 $.getJSON("http://localhost:8080/search_date?date=13.03.2014",function(data){                
-          var taulu = "<ul>";
+        var taulu = "<ol>";
+        var apu = ""; 
             for( i=0 ; i < data.result.length ; i++ )
-              {
-                taulu += data.result[i].liike+ "<br>";
-                  taulu += "<li>" + data.result[i].toistot + " x " + data.result[i].painot + "kg" + "<br>";
-              }
-              taulu += "</ul>";
-          document.getElementById("viimeisin_treeni").innerHTML=taulu;
+            	{
+            	if( data.result[i].liike == apu )
+            		{
+            		taulu += "<ol>" + data.result[i].toistot + " x " + data.result[i].painot + "kg" + "<br>";
+            		}
+            	else
+            		{
+            		apu = data.result[i].liike;
+            		taulu += "<br>" + data.result[i].liike+ "<br>";
+                  	taulu += "<ol>" + data.result[i].toistot + " x " + data.result[i].painot + "kg" + "</ol>";
+            		}
+       			}
+            taulu += "</ol>";
+          	document.getElementById("viimeisin_treeni").innerHTML=taulu;
         });
